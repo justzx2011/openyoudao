368c7780dcfb4ce3e1e5b200ac90072bef87b2b6
justzx <justzx2011@gmail.com>
2012-06-15 14:25:15 +0800

    设置配置文件

---
+++ sqlite.py
@@ -0,0 +1,22 @@
+import sqlite3
+import sys
+import StringIO
+import os
+conn = sqlite3.connect('/home/justzx/.local/share/webkit/databases/file__0.localstorage')
+c = conn.cursor()
+c.execute("select value from ItemTable where key = 'dict' ")
+r= c.fetchone()
+output=StringIO.StringIO()
+print >> output,r[0]
+url=""
+#print output.getvalue()
+#print "SQLite version: %s" % output.getvalue()
+#text1=''.join(output)
+#print text1
+url=str(output.getvalue())
+#print url >> cache/history.cache
+#os.system("/bin/echo -e  \'"+ url  + "\' >> cache/history.cache")
+oldStdout = sys.stdout
+sys.stdout= open("cache/history.cache", "w+")
+print >> sys.stdout,url.strip()
+output.close()
--- youdao.py
+++ youdao.py
@@ -1,4 +1,6 @@
 #encoding=utf-8 
+import sqlite3
+import sys
 import webkit, gtk
 import thread
 import fusion
@@ -6,8 +8,43 @@ import webshot
 import record_xclip
 import random
 import os
+import StringIO
 import time
 from time import sleep
+def inputconfig():
+    global keywordtext
+    keywordtext = ""
+    global baseurl
+    baseurl=""
+    global prebaseurl
+    prebaseurl=""
+    prebaseurl = baseurl
+    conn = sqlite3.connect('/home/justzx/.local/share/webkit/databases/file__0.localstorage')
+    homedir = os.getcwd()
+    c = conn.cursor()
+    c1 = conn.cursor()
+    c.execute("select value from ItemTable where key = 'dict' ")
+    c1.execute("select value from ItemTable where key = 'keyword' ")
+    r= c.fetchone()
+    r1= c1.fetchone()
+    output=StringIO.StringIO() 
+    output1=StringIO.StringIO() 
+    print >>output,r[0]
+    print >>output1,r1[0]
+    print baseurl,output.getvalue()
+    print keywordtext,output1.getvalue()
+    baseurl=str(output.getvalue())
+    keywordtext=str(output1.getvalue())
+    if(prebaseurl != baseurl):
+        text=str(output1.getvalue())
+        #print keywordtext
+        #os.system("/bin/echo -e  \'"+ keywordtext + "\' >> cache/history.cache")
+        oldStdout = sys.stdout
+        sys.stdout= open("cache/history.cache", "w+")
+        print >> sys.stdout,url.strip()
+    prebaseurl=baseurl
+    output.close()
+    output1.close()
 def lookup():
     sleep(5)
     global homedir
@@ -16,16 +53,18 @@ def lookup():
     text=""
     global url
     url=""
+    global baseurl
     #监视history.txt变化
     cmd = "tail -f " + homedir + "/cache/history.cache"
-    #print cmd
+    inputconfig()
+    print baseurl
     #myfile=os.popen("tail -f \'" + homedir +"\' + \'"/cache/history.txt"\'")
     myfile=os.popen(cmd)
     while True:
         text=myfile.readline().strip('\r\n\x00')
         if (pre_text != text): 
             pre_text = text
-            url="http://dict.youdao.com/search?q=" + text
+            url= baseurl + text
             #tmp="curl -s -o " + homedir + "/cache/youdao.htm \'" + url+ "\'"
             #print tmp
             os.system("curl -s -o cache/youdao.html \'" + url+ "\'")
