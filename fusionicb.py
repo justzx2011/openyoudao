55ce26db1bb16c42126e2c08751f4ca2968c0ad2
justzx <justzbn2011@gmail.com>
2012-06-30 01:58:44 +0800

    完善icb

--- fusionicb.py
+++ fusionicb.py
@@ -8,7 +8,7 @@ def reconstruct():
     soup = BeautifulSoup(open(homedir +  "/cache/origin.html"))
     head=open('cache/construction/icb/head.html','r') 
     bodystart=open('cache/construction/icb/body-start.txt','r') 
-    #bodyend=open('cache/construction/icb/body-end.txt','r') 
+    bodyend=open('cache/construction/icb/body-end.txt','r') 
     result = soup.find('div',{"id":"main_box"})
     #sousuo = soup.find('div',{"class":"search_box"})
     f_tar=open('cache/result.html','w+')
@@ -21,12 +21,12 @@ def reconstruct():
     #print >> f_tar,sousuo
     #print >> f_tar,"</div>"
     print >> f_tar,result
-    #print >> f_tar,bodyend.read()
+    print >> f_tar,bodyend.read()
     print >> f_tar,"</body></html>"
     f_tar.close()
     head.close()
     bodystart.close()
-    #bodyend.close()
+    bodyend.close()
     #os.system("sed -i -e 's/action=\"\/search/action=\"http:\/\/dict.youdao.com\/search/g' cache/result.html")
     #os.system("sed -i -e 's/href=\"\/example/href=\"http:\/\/dict.youdao.com\/example/g' cache/result.html")
     #os.system("sed -i -e 's/<\/div><\/div><\/div>/ /g' cache/result.html")
