6c5d19d3f746300900f6a2ccf87ef8ab5b806469
root <root@debian-i686.example.org>
2012-08-21 17:24:25 +0800

    将临时文件权限设置为666，解决了debian包fixperm无法赋予777的问题

--- ppa/install-openyoudao.sh
+++ ppa/install-openyoudao.sh
@@ -8,7 +8,7 @@ install -g users -o root -v -m 755 -t debian/openyoudao/usr/bin      scripts/ope
 install -g users -o root -v -m 644 -t debian/openyoudao/usr/lib/openyoudao ./*.py
 install -g users -o root -v -m 644 -t debian/openyoudao/usr/share/applications  desktop/openyoudao.desktop
 cp -rf cache/* debian/openyoudao/var/cache/openyoudao
-chmod -R 777 debian/openyoudao/var/cache/openyoudao/*
+chmod -R 666 debian/openyoudao/var/cache/openyoudao/*
 #chmod 777 debian/openyoudao/var/cache/openyoudao/result.html
 #chmod 777 debian/openyoudao/var/cache/openyoudao/origin.html
 #chmod 777 debian/openyoudao/var/cache/openyoudao/history.cache
