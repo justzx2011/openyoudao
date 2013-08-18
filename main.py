36bb5ebc391e421ec693322f1bd79e1e45b09e99
justzx <justzx2011@gmail.com>
2013-08-18 11:23:43 +0800

    取出测边框

--- main.py
+++ main.py
@@ -18,11 +18,11 @@ from Xlib.ext import record
 from Xlib.protocol import rq
 local_dpy = display.Display()
 record_dpy = display.Display()
-def lookup_keysym(keysym):
-  for name in dir(XK):
-    if name[:3] == "XK_" and getattr(XK, name) == keysym:
-      return name[3:]
-    return "[%d]" % keysym
+#def lookup_keysym(keysym):
+#  for name in dir(XK):
+#    if name[:3] == "XK_" and getattr(XK, name) == keysym:
+#      return name[3:]
+#    return "[%d]" % keysym
 
 def record_callback(reply):
     if reply.category != record.FromServer:
@@ -55,26 +55,26 @@ def record_callback(reply):
 			         gl.homeurl="file://" + gl.resultdir #合成最终缓冲访问地址
 			         window.load(gl.homeurl)
 			         window.show()
-if not record_dpy.has_extension("RECORD"):
-  print "RECORD extension not found"
-  sys.exit(1)
-  r = record_dpy.record_get_version(0, 0)
-  print "RECORD extension version %d.%d" % (r.major_version, r.minor_version)
-# Create a recording context; we only want key and mouse events
-ctx = record_dpy.record_create_context(
-0,
-[record.AllClients],
-[{
-'core_requests': (0, 0),
-'core_replies': (0, 0),
-'ext_requests': (0, 0, 0, 0),
-'ext_replies': (0, 0, 0, 0),
-'delivered_events': (0, 0),
-'device_events': (X.KeyPress, X.MotionNotify),
-'errors': (0, 0),
-'client_started': False,
-'client_died': False,
-}])
+#if not record_dpy.has_extension("RECORD"):
+#  print "RECORD extension not found"
+#  sys.exit(1)
+#  r = record_dpy.record_get_version(0, 0)
+#  print "RECORD extension version %d.%d" % (r.major_version, r.minor_version)
+## Create a recording context; we only want key and mouse events
+#ctx = record_dpy.record_create_context(
+#0,
+#[record.AllClients],
+#[{
+#'core_requests': (0, 0),
+#'core_replies': (0, 0),
+#'ext_requests': (0, 0, 0, 0),
+#'ext_replies': (0, 0, 0, 0),
+#'delivered_events': (0, 0),
+#'device_events': (X.KeyPress, X.MotionNotify),
+#'errors': (0, 0),
+#'client_started': False,
+#'client_died': False,
+#}])
 def webshow():
   global window
   global Alive
@@ -82,9 +82,30 @@ def webshow():
   window.load(gl.homeurl)
   window.show()
   gtk.main()
+  record_dpy.record_free_context(ctx)
   Alive=0
 
 def gettext():
+  if not record_dpy.has_extension("RECORD"):
+    print "RECORD extension not found"
+    sys.exit(1)
+    r = record_dpy.record_get_version(0, 0)
+    print "RECORD extension version %d.%d" % (r.major_version, r.minor_version)
+  # Create a recording context; we only want key and mouse events
+  ctx = record_dpy.record_create_context(
+  0,
+  [record.AllClients],
+  [{
+  'core_requests': (0, 0),
+  'core_replies': (0, 0),
+  'ext_requests': (0, 0, 0, 0),
+  'ext_replies': (0, 0, 0, 0),
+  'delivered_events': (0, 0),
+  'device_events': (X.KeyPress, X.MotionNotify),
+  'errors': (0, 0),
+  'client_started': False,
+  'client_died': False,
+  }])
   os.system("xclip -f /dev/null")           #清空剪切板
   record_dpy.record_enable_context(ctx,record_callback)
   record_dpy.record_free_context(ctx)
