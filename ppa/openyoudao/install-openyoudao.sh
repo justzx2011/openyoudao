859dfc2cffc2df54eff342f4a0146ed5080005fd
justzx <justzx2011@gmail.com>
2013-08-31 13:23:28 +0800

    完成ppa打包

--- ppa/openyoudao/install-openyoudao.sh
+++ ppa/openyoudao/install-openyoudao.sh
@@ -2,7 +2,7 @@
 #small script to install openyoudao
 install -g users -o root -m 755 -d debian/openyoudao/usr/bin
 install -g users -o root -m 755 -d debian/openyoudao/usr/lib/openyoudao
-install -g users -o root -m 755 -d debian/openyoudao/usr/share/openyoudao
+install -g users -o root -m 777 -d debian/openyoudao/usr/share/openyoudao
 install -g users -o root -m 755 -d debian/openyoudao/usr/share/applications
 install -g users -o root -v -m 755 -t debian/openyoudao/usr/bin      scripts/openyoudao
 install -g users -o root -v -m 644 -t debian/openyoudao/usr/lib/openyoudao ./*.py
