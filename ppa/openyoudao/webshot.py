e6072c5c079f09d3990389317e4df48e5b8e5747
justzx <justzx2011@gmail.com>
2013-09-01 22:03:31 +0800

    the install branch of ppa and aur

---
+++ ppa/openyoudao/webshot.py
@@ -0,0 +1,61 @@
+import sys
+import gl
+import os
+import gtk
+import time
+import webkit
+class OutputView(webkit.WebView):
+    '''a class that represents the output widget of a conversation
+    '''
+    def __init__(self):
+        webkit.WebView.__init__(self)
+        self.load_finish_flag = False
+        self.set_property('can-focus', True)
+        self.set_property('can-default', True)
+        self.set_full_content_zoom(1)
+       # self.clipbord = gtk.Clipboard()
+        #settings = self.get_settings()
+        #try:
+        #    settings.set_property('enable-universal-access-from-file-uris', True)
+        #    settings.set_property('javascript-can-access-clipboard', False)
+        #    settings.set_property('enable-default-context-menu', True)
+        #    settings.set_property('enable-page-cache', True)
+        #    settings.set_property('tab-key-cycles-through-elements', True)
+        #    settings.set_property('enable-file-access-from-file-uris', True)
+        #    settings.set_property('enable-spell-checking', False)
+        #    settings.set_property('enable-caret-browsing', False)
+        #    try:
+        #         # Since 1.7.5
+        #        settings.set_property('enable-accelerated-compositing', True)
+        #    except TypeError:
+        #         pass
+        #except:
+        #    print 'Error: settings property was not set.'
+
+
+class Window(gtk.Window):
+    def __init__(self):
+        gtk.Window.__init__(self)
+        self.set_resizable(True)
+        self.set_title("openyoudao")
+        self.set_default_size(800, 240)
+        self.scroll = gtk.ScrolledWindow()
+        self.scroll.props.hscrollbar_policy = gtk.POLICY_NEVER
+        self.scroll.props.vscrollbar_policy = gtk.POLICY_NEVER
+        self.output = OutputView()
+        self.scroll.add(self.output)
+        self.add(self.scroll)
+        self.scroll.show_all()
+        self.connect('delete-event', gtk.main_quit)
+        #self.is_fullscreen = False
+    def load(self, url):
+        print url
+        self.output.load_uri(url)
+    def reload(self):
+        self.output.reload()
+        
+#window = Window()
+#window.load(sys.argv[1])
+#window.load("http://dict.youdao.com/")
+#window.show()
+#gtk.main()
---
+++ ppa/openyoudao_0.1-0ubuntu0.dsc
@@ -0,0 +1,35 @@
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA1
+
+Format: 3.0 (quilt)
+Source: openyoudao
+Binary: openyoudao
+Architecture: any
+Version: 0.1-0ubuntu0
+Maintainer: Xin Zhang <justzx2011@gmail.com>
+Homepage: http://openyoudao.org
+Standards-Version: 3.9.3
+Build-Depends: debhelper (>= 8.0.0)
+Package-List: 
+ openyoudao deb net optional
+Checksums-Sha1: 
+ f25d29ef6ba958ab97d07d0adfe17b586028903e 374686 openyoudao_0.1.orig.tar.gz
+ b6e855fc7e8ac40a4f2a3f9f0827054f1cef04af 2078 openyoudao_0.1-0ubuntu0.debian.tar.gz
+Checksums-Sha256: 
+ ebd8f617abc2958149902a658a0ca434a81c2020a8d57f14032d29c22431086c 374686 openyoudao_0.1.orig.tar.gz
+ e7a6bf7a26d733f45253e3b9e71a61ecbb51bd417cd9909d93652233d69347d2 2078 openyoudao_0.1-0ubuntu0.debian.tar.gz
+Files: 
+ 16c5dda6ca5eb93d71537fa76bf535a8 374686 openyoudao_0.1.orig.tar.gz
+ eed4169b04208111fa0440466bbcbb52 2078 openyoudao_0.1-0ubuntu0.debian.tar.gz
+
+-----BEGIN PGP SIGNATURE-----
+Version: GnuPG v1.4.12 (GNU/Linux)
+
+iQEcBAEBAgAGBQJSIzs0AAoJEDTrwpTlb4fZj0sIAJxDmW2pZAQqK24c+pO24QjB
+VfKFgmMR0RqePsPLi+8MRxaiAMRl4FUkD+gX84LgrOt/tyA36xkF0hrDiPWkNQCg
+0F0wIVKR2+zpXmtfVr1z4St53NCK9rIGTJ73HPQIZZujF/BYBFjmmfPfSb53oqI6
+FsLTClZ/X0Y1L4zEuQebE25Wbcuo/zxZ/Fyq6TxGirFHy2CVvOCgrFAL437bFLAD
+AsPWn8eXTwVVVaNnSKAW15/LZ839Gc0taW9jvG9jQT8w7Eor2PeMQPxAZc9yESs0
+qWom+PmmB4FKzqz/Mp1GjV5Cmp/MtIaoXYuh+PQHUHEsbIL5SiYyeePRqA+O2xY=
+=N3LT
+-----END PGP SIGNATURE-----
