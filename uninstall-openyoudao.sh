a89e1bf4c40e882e9010a87112b044448af2ed30
justzx <justzx2011@gmail.com>
2013-09-01 22:06:20 +0800

    the install branch of ppa and aur

--- uninstall-openyoudao.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -vf /usr/bin/openyoudao
-rm -vf /usr/bin/install-openyoudao.sh
-rm -rvf /usr/lib/openyoudao
-rm -vf /usr/share/applications/openyoudao
-rm -rvf /usr/share/openyoudao
-rm -vf /usr/bin/uninstall-openyoudao.sh
