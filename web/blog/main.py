ebf8e3519d5fd0bb968ac4067823c4781a2ffad4
justzx2011 <justzx2011@gmail.com>
2013-09-26 12:52:54 -0400

    update donate

--- web/blog/main.py
+++ web/blog/main.py
@@ -13,5 +13,36 @@ from tornado.options import define, options
 define("port", default=8081, help="run on the given port", type=int)
 define("mysql_host", default="localhost:3306", help="database host")
 define("mysql_database", default="blog", help="database name")
-define("mysql_user", default="blog@youdao", help="database user")
-define("mysql_password", default="youdao@163", help="database password")
+define("mysql_user", default="root", help="database user")
+define("mysql_password", default="", help="database password")
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+             (r"/", HomeHandler),
+        ]
+        settings = dict(
+            urlcheck_title=u"URL",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            debug=True,
+            ) 
+        tornado.web.Application.__init__(self, handlers, **settings)
+        self.db = torndb.Connection(
+            host=options.mysql_host, database=options.mysql_database,
+            user=options.mysql_user, password=options.mysql_password) 
+class BaseHandler(tornado.web.RequestHandler):
+    @property
+    def db(self):
+        return self.application.db
+class HomeHandler(BaseHandler):
+    def get(self):
+	info="blog"
+        self.render("index.html",info=info) 
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port,address='108.174.49.132')
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
