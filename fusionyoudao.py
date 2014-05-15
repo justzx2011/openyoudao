67e5fee512b550ed2df382b5fd1456bf0ce34554
justzx2011 <justzx2011@gmail.com>
2014-05-16 01:09:14 +0800

    clear redirect

--- fusionyoudao.py
+++ fusionyoudao.py
@@ -31,6 +31,13 @@ def reconstruct():
     os.system("sed -i -e 's/href=\"\/simplayer.swf/href=\"http:\/\/dict.youdao.com\/simplayer.swf/g' \'"+ gl.resultdir + "\'")
     #os.system("sed -i -e 's/href=\"\/simplayer.swf/href=\"http:\/\/dict.youdao.com\/simplayer.swf/g' \'"+ gl.resultdir + "\'")
     os.system("sed -i -e 's/<h3>目录<\/h3>/<h3>%index<\/h3>/g' \'"+ gl.resultdir + "\'")
+    os.system("sed -i -e 's/详细内容//g' \'"+ gl.resultdir + "\'")
+    os.system("sed -i -e 's/更多双语例句//g' \'"+ gl.resultdir + "\'")
+    os.system("sed -i -e 's/更多原声例句//g' \'"+ gl.resultdir + "\'")
+    os.system("sed -i -e 's/更多权威例句//g' \'"+ gl.resultdir + "\'")
+    os.system("sed -i -e '/onmousedown/'d \'"+ gl.resultdir + "\'")
+    os.system("sed -i -e '/百度百科/'d \'"+ gl.resultdir + "\'")
+    os.system("sed -i -e 's/<li class=\"nav-collins\"><a href=\"http:\/\/dict.youdao.com\/writing\/?keyfrom=dictweb\" hidefocus=\"true\">英文写作助手<\/a><span class=\"collins-icon\"><\/span><\/li>//g' \'"+ gl.resultdir + "\'")
     #os.system("sed -i -e 's/http:\/\/dict.youdao.com\/writing\/?keyfrom=dictweb/file:\/\/\/usr\/share\/openyoudao\/config.html/g' \'"+ gl.resultdir + "\'")
     print "fusionyoudao completed"
     #os.system("sed -i -e 's/<\/div><\/div><\/div>/ /g' cache/result.html")
